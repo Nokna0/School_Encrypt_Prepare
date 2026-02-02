81d97dc563489822a51f555cb33b23c0460b8273
Nokna <118613153+Nokna0@users.noreply.github.com>
2026-02-02 17:54:34 +0900

    Update Hash Function.py

--- Hash Function.py
+++ Hash Function.py
@@ -6,23 +6,7 @@ def hash(text):
 
     return hash_value
 
-def unhash(hash_value):
-    result = ""
-    remaining = hash_value
-    i = 1
-    
-    while remaining > 0 and i < 256:
-        if i != 0:
-            char_code = remaining // i
-            if 32 <= char_code <= 126:
-                result += chr(char_code)
-                remaining -= char_code * i
-        i += 1
-    
-    return result
 
 target = input()
 
 print(hash(target))
-
-print(unhash(hash(target)))

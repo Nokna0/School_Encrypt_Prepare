b1c151333f55d308844413655a5e87570f993ed7
Nokna <118613153+Nokna0@users.noreply.github.com>
2026-02-02 17:43:12 +0900

    함수 unhash 추가 및 입력 해시값 복원 기능 구현

--- Hash Function.py
+++ Hash Function.py
@@ -6,5 +6,21 @@ def hash(text):
 
     return hash_value
 
+def unhash(hash_value):
+    result = ""
+    remaining = hash_value
+    i = 1
+    
+    while remaining > 0 and i < 256:
+        if i != 0:
+            char_code = remaining // i
+            if 32 <= char_code <= 126:
+                result += chr(char_code)
+                remaining -= char_code * i
+        i += 1
+    
+    return result
 
 print(hash(input()))
+
+print(unhash(hash(input())))

d8c843ef2f88beb20ad6b85693eb35b349266102
Nokna <118613153+Nokna0@users.noreply.github.com>
2026-02-02 17:54:03 +0900

    Update Hash Function.py

--- Hash Function.py
+++ Hash Function.py
@@ -2,7 +2,7 @@ def hash(text):
     hash_value = 0
 
     for i, char in enumerate(text):
-        hash_value += ord(char) * (i + 1)
+        hash_value += ord(char) * i
 
     return hash_value
 
@@ -12,18 +12,17 @@ def unhash(hash_value):
     i = 1
     
     while remaining > 0 and i < 256:
-        char_code = remaining // i
-        if 32 <= char_code <= 126:
-            result += chr(char_code)
-            remaining -= char_code * i
+        if i != 0:
+            char_code = remaining // i
+            if 32 <= char_code <= 126:
+                result += chr(char_code)
+                remaining -= char_code * i
         i += 1
     
     return result
 
-target = input("Enter text to hash: ")
+target = input()
 
-hashed_value = hash(target)
+print(hash(target))
 
-print(hashed_value)
-
-print(unhash(hashed_value))
+print(unhash(hash(target)))

a3d1e6558eb67062205368a4587a2ebd150101a4
Nokna <118613153+Nokna0@users.noreply.github.com>
2026-02-04 19:49:32 +0900

    Fix hash calculation in Block class
    Refactor Block class to correctly calculate hash and update comments.

--- Super Simple Blockchain.py
+++ Super Simple Blockchain.py
@@ -4,34 +4,39 @@ import hashlib
 
 class Block: # 스스로 불러온 재앙에 짓눌려
     def __init__(self, index, previous_hash, timestamp, data, nonce=0):
-      self.index = index
-      self.previous_hash = previous_hash
-      self.timestamp = timestamp
-      self.data = data
-      self.nonce = nonce
-      self.hash = self.calculate_hash(
-  '''
-  모든 정보를 묶고, 해시값으로 바꾼다
-  [[채굴하는 코드]]
-  
-  '''
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+        
+    def calculate_hash(self):
+        block_string = f"{self.index}{self.previous_hash}{self.timestamp}{self.data}{self.nonce}"
+        return hashlib.sha256(block_string.encode()).hexdigest()  
+
   
 class Blockchain:
-  pass # 블록체인이라는 클래스를 만들고 그걸 프리셋으로? 만든다
-  '''
-  블록체인 초기화
-  제네시스 블록 생성
-  가장 최근에 추가한 블록을 가져온다
-  새로운 블록을 추가한다
-  블록체인의 유효성을 검증한다
-  블록체인의 데이터를 출력한다
-  '''
-
-blockchain = Blockchain()
-block = Block()
-
-# 여기에 코드를 작성한다
-# 테스트하는 과정을 여기에 코드로 구현하고 위에 클래스에 그 과정을 전부 만든다
+    pass # 블록체인이라는 클래스를 만들고 그걸 프리셋으로? 만든다
+'''
+블록체인 초기화
+제네시스 블록 생성
+가장 최근에 추가한 블록을 가져온다
+새로운 블록을 추가한다
+블록체인의 유효성을 검증한다
+블록체인의 데이터를 출력한다
+'''
+          
+if __name__ == "__main__":
+    my_blockchain = Blockchain()
+    
+    my_blockchain.add_block("A가 B에게 10 코인 전송")
+    my_blockchain.add_block("B C에게 5 코인 전송")
+    my_blockchain.add_block("C가 A에게 3 코인 전송")
+    
+    my_blockchain.display_chain()
+    
+    print(f"\n블록체인 유효성: {my_blockchain.is_chain_valid()}")
 
 '''
 <<<블록체인을 시뮬레이션하는 코드>>>

36b15ef6ea41654c35acdde5f55459313cf3002a
Nokna <118613153+Nokna0@users.noreply.github.com>
2026-02-04 17:54:44 +0900

    Update Super Simple Blockchain.py

--- Super Simple Blockchain.py
+++ Super Simple Blockchain.py
@@ -2,8 +2,15 @@ import time
 import json
 import hashlib
 
-def Block():
-  pass
+class Block:
+  pass # 블록이라는 클래스를 만들고 그걸 프리셋으로 정한다
 
-def Blockchain():
-  pass
+class Blockchain:
+  pass # 블록체인이라는 클래스를 만들고 그걸 프리셋으로? 만든다
+
+blockchain = Blockchain()
+block = Block()
+
+# 여기에 코드를 작성한다
+# 테스트하는 과정을 여기에 코드로 구현하고 위에 클래스에 그 과정을 전부 만든다
+
